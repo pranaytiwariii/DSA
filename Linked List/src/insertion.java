64ff6bd388f6f6571bd0effbb2ec160f9ac83075
pranaytiwariii <pranaytiwariprpk@gmail.com>
2023-10-02 21:19:02 +0530

    [ADDED]linked list

---
+++ Linked List/src/insertion.java
@@ -0,0 +1,64 @@
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedList {
+    private Node head;
+
+    // Insertion at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insertion at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Insertion after a given node
+    public void insertAfter(Node prevNode, int data) {
+        if (prevNode == null) {
+            System.out.println("Previous node cannot be null.");
+            return;
+        }
+        Node newNode = new Node(data);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+    }
+
+    // Print the linked list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertAtBeginning(1);
+        linkedList.insertAtEnd(3);
+        linkedList.insertAfter(linkedList.head, 2);
+
+        System.out.println("Linked List: ");
+        linkedList.printList();
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayer;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    private void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkWin() {
+        // Check rows, columns, and diagonals
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true; // Check rows
+            }
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true; // Check columns
+            }
+        }
+        if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+            return true; // Check diagonals
+        }
+        if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+            return true; // Check diagonals
+        }
+        return false;
+    }
+
+    public void playGame() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Current board:");
+            printBoard();
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            // Check if the move is valid
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+                board[row][col] = currentPlayer;
+                if (checkWin()) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    printBoard();
+                    break;
+                } else if (isBoardFull()) {
+                    System.out.println("The game is a draw!");
+                    printBoard();
+                    break;
+                } else {
+                    // Switch player
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.playGame();
+    }
+}
